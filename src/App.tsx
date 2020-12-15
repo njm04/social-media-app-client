b3e31298801b626baf814999c22d6cfbd9cccde5
Neil John Medalla <medallaneiljohn@gmail.com>
2020-12-15 13:58:44 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const name: string = "Neil";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. {name}
         </p>
         <a
           className="App-link"
