04fb39d1441bb498982f4049a29c4ea64d77d931
Neil John Medalla <medallaneiljohn@gmail.com>
2020-12-21 22:18:27 -0700

    create getDate util func

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,13 @@
+import moment from "moment";
+
 export const getInitials = (name: string) => {
   return name
     .split(" ")
     .map((initial) => initial.charAt(0))
     .join("")
     .toUpperCase();
+};
+
+export const getDate = (date: string) => {
+  return moment(date).fromNow();
 };
