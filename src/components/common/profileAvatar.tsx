d9db36067cf2fac82b57fef3412523db916d964e
Neil John Medalla <medallaneiljohn@gmail.com>
2021-02-19 18:55:13 -0700

    added badges on avatars to determine if user is active or not

--- src/components/common/profileAvatar.tsx
+++ src/components/common/profileAvatar.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
 import Avatar from "@material-ui/core/Avatar";
-import { getProfilePicture } from "../../store/users";
+import { getProfilePicture, getUser } from "../../store/users";
 import { getInitials } from "../../utils/utils";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    customBadge: {
+      backgroundColor: "#44b700",
+    },
+  })
+);
 
 export interface ProfileAvatarProps {
   userId: string;
@@ -16,23 +26,78 @@ const ProfileAvatar: React.FC<ProfileAvatarProps> = ({
   fullName,
   handleProfileOpen,
 }: ProfileAvatarProps) => {
+  const classes = useStyles();
+  const user = useSelector(getUser)(userId);
   const avatar = useSelector(getProfilePicture)(userId);
 
-  return avatar && handleProfileOpen ? (
-    <IconButton color="inherit" onClick={() => handleProfileOpen(userId)}>
-      <Avatar alt={avatar.name} src={avatar.url} />
-    </IconButton>
-  ) : handleProfileOpen ? (
-    <IconButton color="inherit" onClick={() => handleProfileOpen(userId)}>
-      <Avatar>{getInitials(fullName)}</Avatar>
-    </IconButton>
-  ) : avatar ? (
-    <Avatar alt={avatar.name} src={avatar.url} />
-  ) : (
-    <IconButton color="inherit">
-      <Avatar>{getInitials(fullName)}</Avatar>
-    </IconButton>
-  );
+  const isOnline = () => {
+    if (user && user.status === "active") return false;
+    return true;
+  };
+
+  const renderAvatar = () => {
+    if (avatar && handleProfileOpen) {
+      return (
+        <IconButton color="inherit" onClick={() => handleProfileOpen(userId)}>
+          <Badge
+            classes={{ badge: classes.customBadge }}
+            overlap="circle"
+            badgeContent=" "
+            invisible={isOnline()}
+            variant="dot"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+          >
+            <Avatar alt={avatar.name} src={avatar.url} />
+          </Badge>
+        </IconButton>
+      );
+    } else if (handleProfileOpen) {
+      return (
+        <IconButton color="inherit" onClick={() => handleProfileOpen(userId)}>
+          <Badge
+            classes={{ badge: classes.customBadge }}
+            overlap="circle"
+            badgeContent=" "
+            invisible={isOnline()}
+            variant="dot"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+          >
+            <Avatar>{getInitials(fullName)}</Avatar>
+          </Badge>
+        </IconButton>
+      );
+    } else if (avatar) {
+      return (
+        <Badge
+          classes={{ badge: classes.customBadge }}
+          overlap="circle"
+          badgeContent=" "
+          invisible={isOnline()}
+          variant="dot"
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+        >
+          <Avatar alt={avatar.name} src={avatar.url} />
+        </Badge>
+      );
+    } else {
+      return (
+        <IconButton color="inherit">
+          <Avatar>{getInitials(fullName)}</Avatar>
+        </IconButton>
+      );
+    }
+  };
+
+  return renderAvatar();
 };
 
 export default ProfileAvatar;
